8f2dd1fc2a507dab0934f785dfae1396276eeb30
Bridget <bridgetnaylor2000@gmail.com>
2019-11-26 13:13:46 -0800

    Method that checks which button is being clicked

--- app/src/main/java/com/example/collegetour/MainActivity.java
+++ app/src/main/java/com/example/collegetour/MainActivity.java
@@ -4,31 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_map);
     }
 
 
     //Runs and show different screens
 
-    //possible way to identify which button was clicked
-//    public void onClick(View v) {
-//        switch(v.getId())
-//        {
-//            case R.id.button_a_id:
-//// handle button A click;
-//                break;
-//            case R.id.button_b_id:
-//// handle button B click;
-//                break;
-//            default:
-//                throw new RuntimeException("Unknow button ID");
-//        }
+
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.dormImageMainMapButton:// handle button A click;
+                Toast.makeText(getApplicationContext(), "Dorms", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.lowerCampusMainMapButton:// handle button B click;
+                Toast.makeText(getApplicationContext(), "Lower Campus", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.gymPoliceMainMapButton:// handle button A click;
+                Toast.makeText(getApplicationContext(), "Gym/Police", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.upperCampusMainMapButton:// handle button A click;
+                Toast.makeText(getApplicationContext(), "Upper Campus", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                throw new RuntimeException("Unknow button ID");
+        }
+    }
 
     //screen navigation methods
     public void showUpperCampusMap (View view){
